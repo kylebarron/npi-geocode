2364101eb9f10afd15ec9fd671a0535285aca7e3
Kyle Barron <kylebarron2@gmail.com>
2018-03-23 15:08:45 -0400

    Use local unzipped files list for openaddress import

--- src/openaddresses_update.py
+++ src/openaddresses_update.py
@@ -11,18 +11,19 @@ Depends: ~/pelias.json
 Outputs: ~/pelias.json
 """
 
-import pandas as pd
 import json
+import glob
 from os.path import expanduser
 
 with open(expanduser('~/pelias.json')) as f:
     pelias_json = json.load(f)
 
-df = pd.read_table('http://results.openaddresses.io/state.txt')
-us_sources = [x for x in df['source'].values if x.startswith('us/')]
-us_sources = [x[:-5] + '.csv' for x in us_sources if x.endswith('.json')]
-
-pelias_json['imports']['openaddresses']['files'] = us_sources
+pelias_json['imports']['openaddresses'].keys()
+oa_data_folder = pelias_json['imports']['openaddresses']['datapath']
+files = glob.glob(oa_data_folder + '/us/**/*.csv')
+files = ['/'.join(x.split('/')[-3:]) for x in files]
+files = sorted(files)
+pelias_json['imports']['openaddresses']['files'] = files
 
 with open(expanduser('~/pelias.json'), 'w') as f:
     json.dump(pelias_json, f, sort_keys=True, indent=4)
